40806131db0dad1226a36503bf253849077449ec
Vitalii Kohut <vkohut@intelliarts.com>
2025-06-04 18:55:28 +0300

    feat: add basic repl

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/apex-woot/monkey-interpreter/cmd"
+	"github.com/apex-woot/monkey-interpreter/dlog"
+	"github.com/apex-woot/monkey-interpreter/repl"
+)
 
 func main() {
-	fmt.Println("IT WORKS")
+	cfg := cmd.ParseCFG()
+	fmt.Printf("%+v\n", cfg)
+	dlog.Configure(cfg.Dlog)
+
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello, %s! THis is a Monkey programming language!\n", user.Username)
+	fmt.Print("Feel free to type in commands!\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
